689a0049cd9d18dc10497cb0f1d8b888b7100ea9
Vlatko Stojkoski <stojvlatko@gmail.com>
2021-05-04 01:12:57 +0200

    Map-based feed in hot posts section

--- src/pages/dashboard/HotPosts.tsx
+++ src/pages/dashboard/HotPosts.tsx
@@ -9,20 +9,59 @@ import { Link } from 'react-router-dom';
 import { Section } from './Section';
 
 export const HotPosts: FC = () => {
-	const [feed, setFeed] = useState<ForumPostProperties[]>([]);
+	const [posts, setPosts] = useState<Map<string, ForumPostProperties>>(
+		new Map()
+	);
+	const [postsOrder, setPostsOrder] = useState<string[]>([]);
 
 	useEffect(() => {
 		(async () => {
-			const feedArr = await getForumPosts({ limit: 2 });
-			setFeed(feedArr);
+			const postsArr = await getForumPosts({ limit: 2 });
+
+			const postsOrderArr: string[] = [];
+
+			const keyValueArr: [string, ForumPostProperties][] = postsArr.map(
+				({ id, ...p }) => {
+					postsOrderArr.push(id);
+					return [id, { id, ...p }];
+				}
+			);
+
+			setPosts(new Map(keyValueArr));
+
+			setPostsOrder(postsOrderArr);
 		})();
 	}, []);
+
+	const updatePostsOrded = () => setPostsOrder([...postsOrder]);
+
+	const handleLike = (id: string) => {
+		const post = posts.get(id);
+
+		if (!post) return;
+
+		posts.set(id, {
+			...post,
+			liked: !post.liked,
+			likes: post.likes + (post.liked ? -1 : 1),
+		});
+
+		updatePostsOrded();
+	};
 
 	return (
 		<Section title='Најжешки теми' icon={<HotIcon boxSize='32px' />}>
 			<VStack gridRowGap={3}>
-				{(feed.length &&
-					feed.map((post, postI) => <ForumPost config={post} key={postI} />)) ||
+				{(postsOrder.length &&
+					postsOrder.map((postId, postIdI) => {
+						const post = posts.get(postId);
+
+						if (!post) return null;
+
+						return (
+							<ForumPost config={post} key={postIdI} onLike={handleLike} />
+						);
+					})) ||
 					null}
 				<Link to='/forum'>
 					<Button colorScheme='brand__brown'>Видете повеќе...</Button>
